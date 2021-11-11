f6c281bba8c92cad466ce879b50bfd2aa2995e40
leopoldkwok <leopoldkwok@gmail.com>
2021-11-11 08:34:58 +0000

    Basic Variable Assignment

---
+++ Very Easy/Basic Variable Assignment.js
@@ -0,0 +1,15 @@
+// Basic Variable Assignment
+// A student learning JavaScript was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code.
+
+// Examples
+// nameString("Mubashir") ➞ "MubashirEdabit"
+
+// nameString("Matt") ➞ "MattEdabit"
+
+// nameString("javaScript") ➞ "javaScriptEdabit"
+
+function nameString(name) {
+  var b = 'Edabit';
+  var result = name + b;
+  return result;
+}
