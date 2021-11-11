5b6723b6e0d24c9fee74a65eb347ae5df4d82e4a
leopoldkwok <leopoldkwok@gmail.com>
2021-11-11 09:02:28 +0000

    Is the String Empty?

---
+++ Very Easy/Is the String Empty?.js
@@ -0,0 +1,12 @@
+// Create a function that returns true if a string is empty and false otherwise.
+
+// Examples
+// isEmpty("") ➞ true
+
+// isEmpty(" ") ➞ false
+
+// isEmpty("a") ➞ false
+
+function isEmpty(s) {
+  return s.length > 0 ? false : true;
+}
