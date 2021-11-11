629dfb6c41b523c62c0d0555272785ed5d66e761
leopoldkwok <leopoldkwok@gmail.com>
2021-11-11 08:49:13 +0000

    Fix the Expression

---
+++ Very Easy/Fix the Expression.js
@@ -0,0 +1,14 @@
+// Fix the Expression
+
+// Fix the code in the Code tab so the function returns true if and only if x is equal to 7.
+
+// Examples
+// isSeven(4) ➞ false
+
+// isSeven(9) ➞ false
+
+// isSeven(7) ➞ true
+
+function isSeven(x) {
+  return x === 7 ? true : false;
+}
