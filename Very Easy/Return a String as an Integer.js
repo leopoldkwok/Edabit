391be3c07e3b1befe79f2209f8a1ecf721f1bfc8
leopoldkwok <leopoldkwok@gmail.com>
2021-11-11 09:07:10 +0000

    Return a String as an Integer

---
+++ Very Easy/Return a String as an Integer.js
@@ -0,0 +1,13 @@
+// Return a String as an Integer
+// Create a function that takes a string and returns it as an integer.
+
+// Examples
+// stringInt("6") ➞ 6
+
+// stringInt("1000") ➞ 1000
+
+// stringInt("12") ➞ 12
+
+function stringInt(str) {
+  return parseInt(str);
+}
