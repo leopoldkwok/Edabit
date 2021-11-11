da708a1e97008a9046e1985ee5033ea1a01910d0
leopoldkwok <leopoldkwok@gmail.com>
2021-11-11 09:29:37 +0000

    Buggy Code (Part 2)

---
+++ Very Easy/Buggy Code (Part 2).js
@@ -0,0 +1,17 @@
+// Buggy Code (Part 2)
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+// Examples
+// maxNum(3, 7) ➞ 7
+
+// maxNum(-1, 0) ➞ 0
+
+// maxNum(1000, 400) ➞ 1000
+
+function maxNum(n1, n2) {
+  if (n1 > n2) {
+    return n1;
+  } else {
+    return n2;
+  }
+}
