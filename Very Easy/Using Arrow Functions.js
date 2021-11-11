b7b57cc2522673cf212c85dd98c7a3bc8e0e5d1d
leopoldkwok <leopoldkwok@gmail.com>
2021-11-11 08:52:42 +0000

    Using Arrow Functions

---
+++ Very Easy/Using Arrow Functions.js
@@ -0,0 +1,15 @@
+// Using Arrow Functions
+
+// Create a function that returns the given argument, but by using an arrow function.
+
+// An arrow function is constructed like so:
+
+// arrowFunc=(/*parameters*/)=>//code here
+// Examples
+// arrowFunc(3) ➞ 3
+
+// arrowFunc("3") ➞ "3"
+
+// arrowFunc(true) ➞ true
+
+const arrowFunc = (num) => num;
