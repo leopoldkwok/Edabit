1b0d1b3237838ac9ac4d50dd7a02155891979dcb
leopoldkwok <leopoldkwok@gmail.com>
2021-11-11 08:30:07 +0000

    Is the Number Less than or Equal to Zero?

---
+++ Very Easy/Is the Number Less than or Equal to Zero?.js
@@ -0,0 +1,12 @@
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+// Examples
+// lessThanOrEqualToZero(5) ➞ false
+
+// lessThanOrEqualToZero(0) ➞ true
+
+// lessThanOrEqualToZero(-2) ➞ true
+
+function lessThanOrEqualToZero(num) {
+  return num <= 0 ? true : false;
+}
