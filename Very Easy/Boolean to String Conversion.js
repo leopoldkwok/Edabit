6fd02b95132a21c9c67dd967a82613b1180cc4b7
leopoldkwok <leopoldkwok@gmail.com>
2021-11-11 10:00:16 +0000

    Boolean to String Conversion

---
+++ Very Easy/Boolean to String Conversion.js
@@ -0,0 +1,11 @@
+// Boolean to String Conversion
+// Create a function that takes a boolean variable flag and returns it as a string.
+
+// Examples
+// boolToString(true) ➞ "true"
+
+// boolToString(false) ➞ "false"
+
+function boolToString(flag) {
+  return `${flag}`;
+}
