d3ad8ae2a650c00d50255ea6c55adb52834741fe
leopoldkwok <leopoldkwok@gmail.com>
2021-11-12 17:02:31 +0000

    Arrow Functions

---
+++ Very Easy/Arrow Functions.js
@@ -0,0 +1,33 @@
+// Arrow Functions
+// In the Code tab you will find code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
+
+// Write five adder functions:
+
+// add2(x) should return 2 + x.
+// add3(x) should return 3 + x.
+// add5(x) should return 5 + x.
+// add7(x) should return 7 + x.
+// add11(x) should return 11 + x.
+// Tips
+// Functions that consists only of a return, can be written as oneliner with an arrow function.
+
+// For example, the code:
+
+// function areSame(a, b) {
+//     return a == b;
+// }
+// Can be simplified to:
+
+// areSame = (a, b) => a == b;
+// Bonus
+// (a, b) => a == b is technically an anonymous function. However, it can be assigned to the identifier areSame and it can then be called using areSame().
+
+add2 = (x) => x + 2;
+
+add3 = (x) => x + 3;
+
+add5 = (x) => x + 5;
+
+add7 = (x) => x + 7;
+
+add11 = (x) => x + 11;
