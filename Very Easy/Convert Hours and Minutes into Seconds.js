8a69118dcce70ba62c6fc7d310eb3a026328c763
leopoldkwok <leopoldkwok@gmail.com>
2021-11-12 10:28:18 +0000

    Convert Hours and Minutes into Seconds

---
+++ Very Easy/Convert Hours and Minutes into Seconds.js
@@ -0,0 +1,13 @@
+// Convert Hours and Minutes into Seconds
+// Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+
+// Examples
+// convert(1, 3) ➞ 3780
+
+// convert(2, 0) ➞ 7200
+
+// convert(0, 0) ➞ 0
+
+function convert(hours, minutes) {
+  return hours * 60 * 60 + minutes * 60;
+}
