b5d9ae89d3658177c6cd3027aa1bd4b6f8f2b35d
leopoldkwok <leopoldkwok@gmail.com>
2021-11-12 14:21:18 +0000

    Concatenate First and Last Name into One String

---
+++ Very Easy/Concatenate First and Last Name into One String.js
@@ -0,0 +1,13 @@
+// Concatenate First and Last Name into One String
+// Given two strings, firstName and lastName, return a single string in the format "last, first".
+
+// Examples
+// concatName("First", "Last") ➞ "Last, First"
+
+// concatName("John", "Doe") ➞ "Doe, John"
+
+// concatName("Mary", "Jane") ➞ "Jane, Mary"
+
+function concatName(firstName, lastName) {
+  return `${lastName}, ${firstName}`;
+}
