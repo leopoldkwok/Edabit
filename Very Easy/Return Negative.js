0bda790ebcc5d24deb7c95e55e0b3313bfb03ccd
leopoldkwok <leopoldkwok@gmail.com>
2021-11-12 15:08:41 +0000

    Return Negative

---
+++ Very Easy/Return Negative.js
@@ -0,0 +1,15 @@
+// Return Negative
+// Create a function that takes a number as an argument and returns negative of that number. Return negative numbers without any change.
+
+// Examples
+// returnNegative(4) ➞ -4
+
+// returnNegative(15) ➞ -15
+
+// returnNegative(-4) ➞ -4
+
+// returnNegative(0) ➞ 0
+
+function returnNegative(n) {
+  return -Math.abs(n);
+}
