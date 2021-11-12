a98046a195aa5e2e4b967646a244811018532636
leopoldkwok <leopoldkwok@gmail.com>
2021-11-12 13:02:12 +0000

    Two Makes Ten

---
+++ Very Easy/Two Makes Ten.js
@@ -0,0 +1,13 @@
+// Two Makes Ten
+// Create a function that takes two arguments. Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 10.
+
+// Examples
+// makesTen(9, 10) ➞ true
+
+// makesTen(9, 9) ➞ false
+
+// makesTen(1, 9) ➞ true
+
+function makesTen(a, b) {
+  return a == 10 || b == 10 || a + b == 10 ? true : false;
+}
