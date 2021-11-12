18c1fe012524bea9dab2c5cb75be0aa8b145495e
leopoldkwok <leopoldkwok@gmail.com>
2021-11-12 13:13:25 +0000

    Check if an Integer is Divisible By Five

---
+++ Very Easy/Check if an Integer is Divisible By Five.js
@@ -0,0 +1,13 @@
+// Check if an Integer is Divisible By Five
+// Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+
+// Examples
+// divisibleByFive(5) ➞ true
+
+// divisibleByFive(-55) ➞ true
+
+// divisibleByFive(37) ➞ false
+
+function divisibleByFive(n) {
+  return n % 5 === 0;
+}
