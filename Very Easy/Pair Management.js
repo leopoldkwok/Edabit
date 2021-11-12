36515e7001aac21ee352d64204077190be20ddb1
leopoldkwok <leopoldkwok@gmail.com>
2021-11-12 13:10:03 +0000

    Pair Management

---
+++ Very Easy/Pair Management.js
@@ -0,0 +1,13 @@
+// Pair Management
+// Given two arguments, return an array which contains these two arguments.
+
+// Examples
+// makePair(1, 2) ➞ [1, 2]
+
+// makePair(51, 21) ➞ [51, 21]
+
+// makePair(512124, 215) ➞ [512124, 215]
+
+function makePair(num1, num2) {
+  return [num1, num2];
+}
