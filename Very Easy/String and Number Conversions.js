f77edd3c12671637750b4d0dd4cf7b6d33624a07
leopoldkwok <leopoldkwok@gmail.com>
2021-11-13 18:28:41 +0000

    String and Number Conversions

---
+++ Very Easy/String and Number Conversions.js
@@ -0,0 +1,19 @@
+// String and Number Conversions
+// You need to create two functions to substitute toString() and parseInt(); A function called intToString() that converts integers into strings and a function called stringToInt() that converts strings into integers.
+
+// Examples:
+// intToString(4) ➞ "4"
+
+// stringToInt("4") ➞ 4
+
+// intToString(29348) ➞ "29348"
+// Notes
+// You will get bonus points if you manage to solve this without using toString, parseInt or parseFloat.
+
+function intToString(num) {
+  return `${num}`;
+}
+
+function stringToInt(num) {
+  return Number(num);
+}
