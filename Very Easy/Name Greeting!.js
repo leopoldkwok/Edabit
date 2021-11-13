ee5d2fd6b50a06969007eb51ea55bb9efc3d04f1
leopoldkwok <leopoldkwok@gmail.com>
2021-11-13 08:29:31 +0000

    Name Greeting!

---
+++ Very Easy/Name Greeting!.js
@@ -0,0 +1,11 @@
+// Name Greeting!
+// Create a function that takes a name and returns a greeting in the form of a string. Don't use a normal function, use an arrow function.
+
+// Examples
+// helloName("Gerald") ➞ "Hello Gerald!"
+
+// helloName("Tiffany") ➞ "Hello Tiffany!"
+
+// helloName("Ed") ➞ "Hello Ed!"
+
+const helloName = (name) => `Hello ${name}!`;
