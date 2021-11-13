6577d8724b88dc410e3ba732438897898310e137
leopoldkwok <leopoldkwok@gmail.com>
2021-11-13 00:06:09 +0000

    Is the String Odd or Even?

---
+++ Very Easy/Is the String Odd or Even?.js
@@ -0,0 +1,17 @@
+// Is the String Odd or Even?
+// Given a string, return true if its length is even or false if the length is odd.
+
+// Examples
+// oddOrEven("apples") ➞ true
+// The word "apples" has 6 characters.
+// 6 is an even number, so the program outputs true.
+
+// oddOrEven("pears") ➞ false
+// "pears" has 5 letters, and 5 is odd.
+// Therefore the program outputs false.
+
+// oddOrEven("cherry") ➞ true
+
+function oddOrEven(s) {
+  return s.length % 2 === 0;
+}
