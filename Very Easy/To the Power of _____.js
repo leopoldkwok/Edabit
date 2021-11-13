808f675f46317ab72786caa7292e3a9a1fbae3f4
leopoldkwok <leopoldkwok@gmail.com>
2021-11-13 00:18:23 +0000

    To the Power of _____

---
+++ Very Easy/To the Power of _____.js
@@ -0,0 +1,13 @@
+// To the Power of _____
+// Create a function that takes a base number and an exponent number and returns the calculation.
+
+// Examples
+// calculateExponent(5, 5) ➞ 3125
+
+// calculateExponent(10, 10) ➞ 10000000000
+
+// calculateExponent(3, 3) ➞ 27
+
+function calculateExponent(num, exp) {
+  return num ** exp;
+}
