cbd4d2872b75e7dc6a191c7e310c01fcc6a922ca
leopoldkwok <leopoldkwok@gmail.com>
2021-11-14 00:38:01 +0000

    Find the Index

---
+++ Very Easy/Find the Index.js
@@ -0,0 +1,15 @@
+// Find the Index
+// Create a function that takes an array and a string as arguments and return the index of the string.
+
+// Examples
+// findIndex(["hi", "edabit", "fgh", "abc"], "fgh") ➞ 2
+
+// findIndex(["Red", "blue", "Blue", "Green"], "blue") ➞ 1
+
+// findIndex(["a", "g", "y", "d"], "d") ➞ 3
+
+// findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple") ➞ 0
+
+function findIndex(arr, str) {
+  return arr.indexOf(str);
+}
