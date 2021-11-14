6c152dd245603f34ca2d88e3df983d9b79f8456e
leopoldkwok <leopoldkwok@gmail.com>
2021-11-14 13:16:17 +0000

    Check if an Array Contains a Given Number

---
+++ Very Easy/Check if an Array Contains a Given Number.js
@@ -0,0 +1,15 @@
+// Check if an Array Contains a Given Number
+// Write a function to check if an array contains a particular number.
+
+// Examples
+// check([1, 2, 3, 4, 5], 3) ➞ true
+
+// check([1, 1, 2, 1, 1], 3) ➞ false
+
+// check([5, 5, 5, 6], 5) ➞ true
+
+// check([], 5) ➞ false
+
+function check(arr, el) {
+  return arr.includes(el);
+}
