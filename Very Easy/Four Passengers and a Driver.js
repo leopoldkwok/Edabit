bfcdeedf9db184ed724e36720b2b70b3cc09b766
leopoldkwok <leopoldkwok@gmail.com>
2021-11-14 13:40:29 +0000

    Four Passengers and a Driver

---
+++ Very Easy/Four Passengers and a Driver.js
@@ -0,0 +1,15 @@
+// Four Passengers and a Driver
+// A typical car can hold four passengers and one driver, allowing five people to travel around. Given n number of people, return how many cars are needed to seat everyone comfortably.
+
+// Examples
+// carsNeeded(5) ➞ 1
+
+// carsNeeded(11) ➞ 3
+
+// carsNeeded(0) ➞ 0
+// Notes
+// It's likely there will be a few people left over and some cars won't be filled to max capacity.
+
+function carsNeeded(n) {
+  return Math.ceil(n / 5);
+}
