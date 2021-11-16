4044e0ce8049bfe59386f889db6feded5cbb07a2
leopoldkwok <leopoldkwok@gmail.com>
2021-11-16 07:41:34 +0000

    Get the File Name

---
+++ Very Easy/Get the File Name.js
@@ -0,0 +1,16 @@
+// Get the File Name
+// Create a function that returns the selected filename from a path. Include the extension in your answer.
+
+// Examples
+// getFilename("C:/Projects/pil_tests/ascii/edabit.txt") ➞ "edabit.txt"
+
+// getFilename("C:/Users/johnsmith/Music/Beethoven_5.mp3") ➞ "Beethoven_5.mp3"
+
+// getFilename("ffprobe.exe") ➞ "ffprobe.exe"
+// Notes
+// Tests will include both absolute and relative paths.
+// For simplicity, all paths will include forward slashes.
+
+function getFilename(path) {
+  return path.split('/').pop();
+}
