b6aacc4dba14f53a45c42f5b59caa160d2849146
leopoldkwok <leopoldkwok@gmail.com>
2021-11-16 21:53:41 +0000

    String or Integer?

---
+++ Very Easy/String or Integer?.js
@@ -0,0 +1,19 @@
+// String or Integer?
+// Create a function that checks if the argument is an integer or a string. Return "int" if it's an integer and "str" if it's a string.
+
+// Examples
+// intOrString(8) ➞ "int"
+
+// intOrString("Hello") ➞ "str"
+
+// intOrString(9843532) ➞ "int"
+// Notes
+// Input will either be an integer or a string.
+
+function intOrString(param) {
+  if (typeof param === 'string') {
+    return 'str';
+  } else {
+    return 'int';
+  }
+}
