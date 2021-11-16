8a07be92f58a30f2b02e777732a71294417d5d1d
leopoldkwok <leopoldkwok@gmail.com>
2021-11-16 07:54:35 +0000

    Invert an Array

---
+++ Very Easy/Invert an Array.js
@@ -0,0 +1,13 @@
+// Invert an Array
+// Create a function that takes an array of numbers arr and returns an inverted array.
+
+// Examples
+// invertArray([1, 2, 3, 4, 5]) ➞ [-1, -2, -3, -4, -5]
+
+// invertArray([1, -2, 3, -4, 5]) ➞ [-1, 2, -3, 4, -5]
+
+// invertArray([]) ➞ []
+
+function invertArray(arr) {
+  return arr.map((num) => (num === 0 ? num : num * -1));
+}
