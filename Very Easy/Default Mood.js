3c536aaa83f0920fb51306de448c597bfcc38fa2
leopoldkwok <leopoldkwok@gmail.com>
2021-11-16 08:03:28 +0000

    Default Mood

---
+++ Very Easy/Default Mood.js
@@ -0,0 +1,13 @@
+// Default Mood
+// Create a function that takes in a current mood and return a sentence in the following format: "Today, I am feeling {mood}". However, if no argument is passed, return "Today, I am feeling neutral".
+
+// Examples
+// moodToday("happy") ➞ "Today, I am feeling happy"
+
+// moodToday("sad") ➞ "Today, I am feeling sad"
+
+// moodToday() ➞ "Today, I am feeling neutral"
+
+function moodToday(mood) {
+  return mood ? `Today, I am feeling ${mood}` : `Today, I am feeling neutral`;
+}
