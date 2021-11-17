75d9851e6c75c1f481928db80bd8c39242afb00d
leopoldkwok <leopoldkwok@gmail.com>
2021-11-17 07:43:44 +0000

    ES6: Destructuring Arrays III

---
+++ Very Easy/ES6: Destructuring Arrays III.js
@@ -0,0 +1,13 @@
+// ES6: Destructuring Arrays III
+// You can assign variables from arrays with destructuring like this:
+
+// const arr = ["eyes", "nose", "lips", "ears"]
+// let [eyes, nose, lips, ears] = arr
+// But you can also skip over items in the array being destructured.
+
+// DO NOT change arr
+// DO NOT USE var [lips] = arr[2]
+// "eyes", "nose", and "ears" should not be assigned to anything
+
+const arr = ["eyes", "nose", "lips", "ears"]
+var [, , lips, /* Create variable lips here and assign it to arr[2] */] = arr
