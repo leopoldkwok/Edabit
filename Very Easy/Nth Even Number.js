47e4b53cc1f6118de389e57ef6a3349592ea55f6
leopoldkwok <leopoldkwok@gmail.com>
2021-11-19 09:29:18 +0000

    Nth Even Number

---
+++ Very Easy/Nth Even Number.js
@@ -0,0 +1,15 @@
+// Nth Even Number
+// Create a function that takes a number n and returns the nth even number.
+
+// Examples
+// nthEven(1) ➞ 0
+// // 0 is first even number
+
+// nthEven(2) ➞ 2
+// // 2 is second even number
+
+// nthEven(100) ➞ 198
+
+function nthEven(n) {
+  return n * 2 - 2;
+}
