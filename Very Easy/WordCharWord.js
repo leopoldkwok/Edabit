0af6f5fa40ffd4e7e1e914f9f4e7a99152674a62
leopoldkwok <leopoldkwok@gmail.com>
2021-11-25 14:26:32 +0000

    WordCharWord

---
+++ Very Easy/WordCharWord.js
@@ -0,0 +1,20 @@
+/* 
+
+WordCharWord
+Create a function that will put the first argument, a character, between every word in the second argument, a string.
+
+Examples
+add("R", "javascript is fun") ➞ "javascriptRisRfun"
+
+add("#", "hello world!") ➞ "hello#world!"
+
+add("#", " ") ➞ "#"
+
+Notes
+Make sure there are no spaces between words when returning the function.
+
+*/
+
+function add(char, str) {
+  return str.replace(/ /gi, char);
+}
